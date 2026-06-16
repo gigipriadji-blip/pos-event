c3e2e7a014ed2f1b244a12e491a790e51c9c1295
Gigi Priadji <gigipriadji@gmail.com>
2026-06-16 10:26:36 +0700

    Update app.js

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ function createGasProxy(successCb, failureCb) {
         fetch(WEB_APP_URL, {
           method: 'POST',
           headers: { 'Content-Type': 'text/plain;charset=utf-8' },
-          body: JSON.stringify({ action: prop, data: args[0] })
+          body: JSON.stringify({ action: prop, data: args })
         })
         .then(r => r.json())
         .then(res => {
